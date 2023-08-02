28e8862e08ccef375a71e3f5fdddffa0550f3728
Parthiv <parthiv.varma@mail.utoronto.ca>
2023-08-02 21:12:56 +0530

    Added the langchain ReAct Agent. Modified the chatbot to be able to answer tougher queries

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,11 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
 from langchain.chains import ConversationChain
-from langchain.memory import ConversationBufferWindowMemory
+from langchain.memory import ConversationSummaryBufferMemory
+from langchain import OpenAI, Wikipedia
+from langchain.agents import initialize_agent, Tool
+from langchain.agents import AgentType
+from langchain.agents.react.base import DocstoreExplorer
 
 from langchain.chains import LLMChain
 
@@ -38,9 +42,15 @@ import pdf2image
 import zipfile
 import io
 
+import lib_platform
+if lib_platform.is_platform_windows:
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
 from streamlit_image_select import image_select
 
-COLLECTION_NAME = "resumes"
+COLLECTION_NAME = "full_resumes"
+COLLECTION_NAME_SUMMARIZED= "summarized_resumes"
+delimiter = "####"
 
 if os.name == 'posix' and os.uname().sysname == 'Linux':
     __import__('pysqlite3')
@@ -52,11 +62,31 @@ if os.name == 'posix' and os.uname().sysname == 'Linux':
 #load_dotenv()
     
 # setting up the database
-conninfo = os.environ['DATABASE_URL']
-db = Database(conninfo)
+#conninfo = os.environ['DATABASE_URL']
+#db = Database(conninfo)
 
-openai_api_key = os.environ['OPENAI_API_KEY']
+#openai_api_key = os.environ['OPENAI_API_KEY']
+#openai.api_key = openai_api_key
+
+db = Database("postgres://u_jkyrhncekqwp2ik:fhnv6el1ibcvwwm@02f7e6f1-1adb-4347-835a-02c74fcccb0e.db.cloud.postgresml.org:6432/pgml_scelnd4epc0lxu4")
+openai_api_key = "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"
 openai.api_key = openai_api_key
+
+#React Langchain setup
+docstore=DocstoreExplorer(Wikipedia())
+
+tools = [
+    Tool(
+        name="Search",
+        func=docstore.search,
+        description="useful for when you need to ask with search"
+    ),
+    Tool(
+        name="Lookup",
+        func=docstore.lookup,
+        description="useful for when you need to ask with lookup"
+    )
+]
 
 # set page title
 st.set_page_config(page_title='Team Byte Busters')
@@ -86,17 +116,23 @@ def preprocess(text):
 # convert pdf to text
 def pdfs_to_documents(files):
     documents = []
+    summarized_documents = []
+    text_to_summarize = ""
     for file in files:
         doc = fitz.open(stream=file.read(), filetype='pdf')
         total_pages = doc.page_count
-
+        text_to_summarize = ""
         for i in range(total_pages):
             text = doc.load_page(i).get_text("text")
             text = preprocess(text)
+            text_to_summarize += text + "\n" 
             documents.append({"text":text,"page number": i, "source":file.name})
+
+        summarized_text = summarizer(text_to_summarize)
+        summarized_documents.append({"text":summarized_text,"source":file.name})
         doc.close()
     
-    return documents
+    return documents, summarized_documents
 
 # generate response from uploaded resume using openai
 #def generate_response(uploaded_files, openai_api_key, query_text):
@@ -139,14 +175,96 @@ def pdfs_to_documents(files):
         #progress_bar.empty()
         
         #return response
+def summarizer(resume_text):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages =  [  
+        {'role':'system',
+        'content':'You are a resume summarizer. An entire resume will be provided to you and you must summarize it within 250 tokens without losing relevant informaiton.\
+            You must keep information such as candidate gpa, candidate name, Work Experience, technical skills, Education, Certifications and Licenses, Projects and Accomplishments, Awards and Honors and Publications and Research if it exists there in the resume'},    
+        {'role':'user',
+        'content':f'{resume_text}'},  
+        ] ,
+        temperature=0.0, # this is the degree of randomness of the model's output
+        max_tokens=250, # the maximum number of tokens the model can ouptut 
+    )
+    #print(response.choices[0].message["content"])
+    return response.choices[0].message["content"]
 
+def categorizer(query_text):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages =  [  
+        {'role':'system',
+        'content':"You are a resume analyzer. \
+                Resume text from a database will be sent to you and you will be asked questions on them.\
+                Classify each query into a primary category. \
+                Primary categories: Resume info gathering, Candidate comparison, \
+                Candidate finding, Contact candidate by email, Schedule meeting with candidate.\
+                Resume info gathering is when the user asks you to find info about candidates from their resume.\
+                Candidate comparison is when user asks you to compare between candidates. Candidate comparison could involve ranking candidates in the database, finding the best fit candidate, checking who the most qualified for a certain job is and other similar questions.\
+                Candidate finding is when user asks you to find candidates from the database with a specific skill or experience.\
+                Contact candidate by email is when user asks you to contact someone\
+                Schedule meeting with candidate is when user asks you to schedule a meeting with candidate\
+                Reply using just the category name."},    
+        {'role':'user',
+        'content':f'{query_text}'},  
+        ] ,
+        temperature=0.0, # this is the degree of randomness of the model's output
+        max_tokens=200, # the maximum number of tokens the model can ouptut 
+    )
+    #print(response.choices[0].message["content"])
+    return response.choices[0].message["content"]
+
+def ranker(query_text):
+    
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages =  [  
+        {'role':'system',
+        'content':f"You are a resume ranker.\
+                Summarized resume text of candidates from a database will be sent to you along with the job details for which they are applying for.\
+                The customer query will be delimited with four hashtags,i.e. {delimiter}.\
+                Step 1: {delimiter}First examine the job details. What would be the best qualifications for the given job?\
+                Step 2: {delimiter}Examine the resumes of the candidates and assign each of them points according to their relative suitability to the job.\
+                Points should be assigned based on the following system:\
+                Relevance to Job Description: out of 10 points\
+                Years of Experience: out of 8 points\
+                Education and Certifications: out 6 points\
+                Key Skills and Expertise: out 9 points\
+                Achievements and Awards: out 7 points\
+                Work History and Career Progression: out of 8 points\
+                Projects and Contributions: out of 7 points\
+                Soft Skills and Interpersonal Abilities: out of 6 points\
+                Add the points together for each candidate\
+                Step 3: {delimiter}Sort the candidates in descending order based on the points assigned.\
+                Step 4: {delimiter}Generate the descending list of candidates with just their names. No other information should be provided.\
+                Use the following format:\
+                Step 1: {delimiter}<step 1 reasoning>\
+                Step 2: {delimiter}<step 2 reasoning>\
+                Step 3: {delimiter}<step 3 reasoning>\
+                Step 4: {delimiter}<step 4 result>\
+                Make sure to include {delimiter} to separate every step.\
+                "},    
+        
+        {'role':'user',
+        'content':f'{query_text}'},  
+        ] ,
+        temperature=0.0, # this is the degree of randomness of the model's output
+    )
+    #print(response.choices[0].message["content"])
+    return (response.choices[0].message["content"]).split(delimiter)[-1].strip()
+  
 def get_conversation():
+    
     llm = ChatOpenAI(openai_api_key=openai_api_key, temperature = 0.0)
     prompt = ChatPromptTemplate(
         messages=[
             SystemMessagePromptTemplate.from_template(
-                "You are a resume analyzer. Resume text from a database will be sent to you and you will be asked to provide information from them."
+                f"You are a resume analyzer. \
+                Resume text from a database will be sent to you and you will be asked questions on them."
             ),
+            
             # The `variable_name` here is what must align with memory
             MessagesPlaceholder(variable_name="chat_history"),
             HumanMessagePromptTemplate.from_template("{input}")
@@ -154,7 +272,7 @@ def get_conversation():
     )
     # Notice that we `return_messages=True` to fit into the MessagesPlaceholder
     # Notice that `"chat_history"` aligns with the MessagesPlaceholder name.
-    memory = ConversationBufferWindowMemory(k = 10, memory_key="chat_history", return_messages=True)
+    memory = ConversationSummaryBufferMemory(llm=llm, max_token_limit=300, memory_key="chat_history", return_messages=True)
     conversation = ConversationChain(
         llm=llm,
         prompt=prompt,
@@ -169,25 +287,31 @@ if conversation_key not in st.session_state:
     
 conversation = st.session_state[conversation_key]
     
-async def database_functions(collection_name, documents, db):
+async def database_functions(collection_name, collection_name_summarized, documents, summarized_documents, db):
     collection = await db.create_or_get_collection(collection_name)
     await collection.upsert_documents(documents)
     
+    collection_summarized = await db.create_or_get_collection(collection_name_summarized)
+    await collection_summarized.upsert_documents(summarized_documents)
     #generating chunks and embeddings
     await collection.generate_chunks()
     await collection.generate_embeddings()
+    
+    await collection_summarized.generate_chunks()
+    await collection_summarized.generate_embeddings()
 
-async def vector_search_function(collection_name, query_text, db):
+async def vector_search_function(k, collection_name, query_text, db):
     collection = await db.create_or_get_collection(collection_name)
-    vector_search_results = await collection.vector_search(query_text, top_k = 3)
+    vector_search_results = await collection.vector_search(query_text, top_k = k)
     context = ""
     for search_result in vector_search_results:
         context += search_result[1] + "\n"
     context += query_text
     return context   
 
-async def delete_all_data(db, collection_name):
+async def delete_all_data(db, collection_name, collection_name_summarized):
     db.archive_collection(collection_name)
+    db.archive_collection(collection_name_summarized)
 
 #Opening note
 
@@ -210,10 +334,12 @@ with st.chat_message("assistant"):
     confirmation = st.checkbox("I understand that this action will delete all data. Confirm?")
     if st.button("Delete All Data") and confirmation:
         with st.spinner('Deleting files from database...'):
-            asyncio.run(delete_all_data(db=db, collection_name=COLLECTION_NAME))
+            asyncio.run(delete_all_data(db=db, collection_name=COLLECTION_NAME, collection_name_summarized= COLLECTION_NAME_SUMMARIZED))
+            #asyncio.run(delete_all_data(db=db, collection_name=COLLECTION_NAME_SUMMARIZED))
             st.success('Database cleared!', icon="🗑️")
 
 st.title("Add resume to database")
+
 
 def on_click():
     st.session_state.user_input = ""
@@ -229,10 +355,12 @@ with st.chat_message("assistant"):
         if add_resume_to_database:
             with st.spinner('Adding files to database...'):
                 uploaded_documents = []
-                uploaded_documents = pdfs_to_documents(uploaded_files)
-                asyncio.run(database_functions(collection_name = COLLECTION_NAME, documents = uploaded_documents, db=db))
+                summarized_resumes = []
+                uploaded_documents, summarized_resumes = pdfs_to_documents(uploaded_files)
+                asyncio.run(database_functions(collection_name = COLLECTION_NAME, collection_name_summarized= COLLECTION_NAME_SUMMARIZED, documents = uploaded_documents, summarized_documents=summarized_resumes, db=db))
+                #asyncio.run(database_functions(collection_name = COLLECTION_NAME_SUMMARIZED, documents = summarized_resumes, db=db))
                 st.success('Files Added!', icon="✅")
-
+                
     buttons = [
         "Clear",
         "Who has the highest GPA?",
@@ -246,6 +374,33 @@ with st.chat_message("assistant"):
     with col1: st.button(buttons[0], on_click=on_click)
     with col2: st.button(buttons[1], on_click=show_highest_gpa)
     with col3: st.button(buttons[2], on_click=show_highest_gpa)
+
+st.title("Compare resumes")
+
+with st.chat_message("assistant"):
+    st.info('Please note, our current database limit for comparisons is 15 resumes.')
+    Job_requirement = st.text_input("Please provide the job requirements for the position that candidates will be evaluated and ranked against.")
+    is_input_given = (Job_requirement.strip() != "")
+    # Button to process the input
+    if st.button("Compare resumes", disabled=not is_input_given):
+        with st.spinner('Comparing...'):
+            query_text = f"Rank the top candidates for {Job_requirement}. If the job requirement does not have enough details for you to make a solid decision, ask the user to provide a more detailed job description. This is the maximum candidate information you can get, so ask for more job details. If the job description is not related to any of the candidate's work experience, let the user know that none of the candidates have enough experience for the particular job. "
+            context_for_summarized_resume = asyncio.run(vector_search_function(15, COLLECTION_NAME_SUMMARIZED, query_text, db))
+            llm = OpenAI(openai_api_key=openai_api_key, temperature=0, model_name="gpt-3.5-turbo")
+            react = initialize_agent(tools, llm, agent=AgentType.REACT_DOCSTORE, verbose=True)
+            try:
+                response = react.run(context_for_summarized_resume)
+            except ValueError as e:
+                response = str(e)
+                if not response.startswith("Could not parse LLM Output: "):
+                   raise e
+                response = response.removeprefix("Could not parse LLM Output: ").removesuffix("`")
+            #response = ranker(query_text=context_for_summarized_resume)
+            st.write(response)
+            
+#st.title("Chatbot instructions") 
+#with st.chat_message("assistant"):
+#    st.title()
 
 # result = []
 # with st.form('Queryform', clear_on_submit=False):
@@ -296,11 +451,22 @@ if query_text := st.chat_input("Ask a question to get information on the resumes
         full_response = ""
         with st.spinner('Thinking...'):
             try:
-                context_for_resume = asyncio.run(vector_search_function(COLLECTION_NAME, query_text, db))
+                category = categorizer(query_text=query_text)
+                context_for_resume = asyncio.run(vector_search_function(3, COLLECTION_NAME, query_text, db))
                 #print(context_for_resume)
+
                 conversation = st.session_state[conversation_key]
-                response = conversation.predict(input = context_for_resume)
-                print(response)
+                #response = conversation.predict(input = context_for_resume)
+                llm = OpenAI(openai_api_key=openai_api_key, temperature=0, model_name="gpt-3.5-turbo")
+                react = initialize_agent(tools, llm, agent=AgentType.REACT_DOCSTORE, verbose=True)
+                try:
+                    response = react.run(context_for_resume)
+                except ValueError as e:
+                    response = str(e)
+                    if not response.startswith("Could not parse LLM Output: "):
+                        raise e
+                    response = response.removeprefix("Could not parse LLM Output: ").removesuffix("`")
+                #print(response)
             except Exception as e: 
                 #response = "Currently, our database does not contain any resumes. We kindly request you to add a resume to our database before proceeding with any questions for the chatbot. Thank you for your cooperation and understanding."
                 response = f"error: {e}"
